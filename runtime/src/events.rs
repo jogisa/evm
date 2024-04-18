be9bbfab805aa544eb1bb5581f3c0b246af49d95
shamasrehan <shamasrehang@gmail.com>
2024-04-18 21:15:30 +0400

    fix-imports

--- runtime/src/events.rs
+++ runtime/src/events.rs
@@ -4,7 +4,7 @@ use super::Context;
 use alloc::vec::Vec;
 use ethereum_types::{H160, H256, U256};
 use crate::{ExitError, ExitReason, ExitSucceed, Opcode};
-use scale_codec::{Decode, Encode};
+use parity_scale_codec::{Decode, Encode};
 
 #[derive(Clone, Debug, Encode, Decode, PartialEq, Eq)]
 pub struct Stack {
